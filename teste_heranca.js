52ec02f227e0ccc1b38857768ecfc5d94a26995b
Rafael Cruz <rafacteixeira@gmail.com>
2017-02-06 22:30:02 -0200

    relembrando herança em JS

---
+++ teste_heranca.js
@@ -0,0 +1,31 @@
+function Person() {
+    this.name = '';
+    this.address = {
+        street: '',
+        number: 0,
+        city: '',
+        country: ''
+    }
+}
+
+Person.prototype.sayName = function() {
+  console.log(this.name);
+};
+
+function IndividualPerson() {
+    this.taxNumber = '';
+}
+IndividualPerson.prototype = Object.create(Person.prototype);
+IndividualPerson.prototype.printCpf = function () {
+    console.log(this.taxNumber);
+};
+
+var rafael = new IndividualPerson();
+rafael.name = 'rafael nome';
+rafael.taxNumber = '083';
+rafael.sayName();
+rafael.printCpf();
+
+var rafael2 = new Person();
+rafael2.name = 'rafael 2';
+rafael2.sayName();
