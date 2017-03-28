4e9e017cc5c7baa77001b28cbb4982229484ce1f
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-27 22:29:18 -0300

    web server lesson 2 - handmade Templates

--- web_server/app.js
+++ web_server/app.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 
 http.createServer(function(req, resp){
     resp.writeHead(200, { 'Content-Type' : 'text/html'});
-    var html = fs.readFileSync(__dirname + '/index.html');
+    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    var message = 'hello world template';
+    html = html.replace('{Message}', message);
     resp.end(html);
 }).listen(1337, '127.0.0.1');
