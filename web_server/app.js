6451c6a747a97d1786fd22ae4009712f4bb69a2e
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-27 23:24:45 -0300

    web server lesson 4 - returning JSON

--- web_server/app.js
+++ web_server/app.js
@@ -2,7 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 
-http.createServer(function(req, resp){
-    resp.writeHead(200, { 'Content-Type' : 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(resp);
+http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type' : 'application/json'});
+    var obj = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
 }).listen(1337, '127.0.0.1');
