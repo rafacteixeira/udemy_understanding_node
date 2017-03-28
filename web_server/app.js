33e9cfda38a1384a2b923137ccab672c52cc7028
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-27 22:22:53 -0300

    web server lesson 2 - Serving files

--- web_server/app.js
+++ web_server/app.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 
 
 http.createServer(function(req, resp){
-    resp.writeHead(200, { 'Content-Type' : 'text/plain'});
-    resp.end('Hello World!\n');
+    resp.writeHead(200, { 'Content-Type' : 'text/html'});
+    var html = fs.readFileSync(__dirname + '/index.html');
+    resp.end(html);
 }).listen(1337, '127.0.0.1');
