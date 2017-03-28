151aec16f536e464266b52e3a40a44e4157ccc64
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-27 23:37:27 -0300

    web server lesson 5 - Manual Routing

--- web_server/app.js
+++ web_server/app.js
@@ -3,10 +3,19 @@ var fs = require('fs');
 
 
 http.createServer(function(req, res){
-    res.writeHead(200, { 'Content-Type' : 'application/json'});
-    var obj = {
-        firstName: 'John',
-        lastName: 'Doe'
-    };
-    res.end(JSON.stringify(obj));
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/api') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var obj = {
+            firstName: 'John',
+            lastName: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 }).listen(1337, '127.0.0.1');
