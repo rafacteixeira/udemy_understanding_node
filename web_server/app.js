23a4ba4d9482260305ca5cdbf34e6e494752814a
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-27 23:16:54 -0300

    web server lesson 3 - writing response using streams

--- web_server/app.js
+++ web_server/app.js
@@ -4,8 +4,5 @@ var fs = require('fs');
 
 http.createServer(function(req, resp){
     resp.writeHead(200, { 'Content-Type' : 'text/html'});
-    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-    var message = 'hello world template';
-    html = html.replace('{Message}', message);
-    resp.end(html);
+    fs.createReadStream(__dirname + '/index.html').pipe(resp);
 }).listen(1337, '127.0.0.1');
