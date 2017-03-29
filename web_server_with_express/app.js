1f711b860cff9b0a7f902034fa89eba991444bf0
Rafael Cruz <rafacteixeira@gmail.com>
2017-03-28 22:25:49 -0300

    serving static files and basic usage of templates

--- web_server_with_express/app.js
+++ web_server_with_express/app.js
@@ -2,12 +2,25 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+    console.log('Request received and filtered');
+    next();
+});
+
 app.get('/', function (req, res) {
-    res.send('<html><head><title>hello express</title></head><body><h1>Hello World!!</h1></body></html>');
+    res.render('index');
 });
 
 app.get('/api', function (req, res) {
     res.json({ firstName:"John", lastName: "Doe"});
+});
+
+app.get('/person/:id', function (req, res) {
+    res.json({ id: req.params.id});
 });
 
 app.listen(port);
